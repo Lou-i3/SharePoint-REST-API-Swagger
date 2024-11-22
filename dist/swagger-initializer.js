b67aed91e3120ccf0b98edb4676d49dc885ce811
Auto Site Save <dewwwe@dewwwe.com>
2024-11-22 16:15:43 +0100

    fix cookies

--- dist/swagger-initializer.js
+++ dist/swagger-initializer.js
@@ -6,6 +6,8 @@ window.onload = function() {
     url: "swagger.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
+    tryItOutEnabled: true,
+    filter: true, // Enable filtering from topbar search
     presets: [
       SwaggerUIBundle.presets.apis,
       SwaggerUIStandalonePreset
@@ -13,7 +15,34 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    persistAuthorization: true,
+    requestInterceptor: (req) => {
+
+      const newReq = {
+        ...req
+      }
+
+      if (req.url.includes("_api")) {
+        // console.log("req interceptor")
+        // console.log('req url: ', req.url)
+        // console.log('req cookie', req.headers.Cookie)
+        // newReq.headers.Cookie = req.headers.Cookie + ";Test=coucou;"
+        if (req.headers.Cookie) {
+          // console.log('rewriting cookies')
+          let properCookies = decodeURIComponent(newReq.headers.Cookie.replace('&', ';'))
+          // newReq.headers['X-HTTP-Cookie'] = properCookies
+          newReq.headers['Cookie'] = properCookies
+          // newReq.headers['X-HTTP-URL'] = newReq.url
+          // newReq.url = '/_api/' + req.url.split('/_api/')[1]
+          // console.log("new Cookie: ", req.headers.Cookie)
+        }
+
+      }
+      newReq.credentials = 'include'
+      return newReq
+    },
+    showMutatedRequest: true
   });
 
   //</editor-fold>
